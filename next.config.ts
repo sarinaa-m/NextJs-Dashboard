e88dd85dd139b6f3e1880c197aefcc98711d4d99
sarinaa-m <sarin.moayedi@gmail.com>
2025-11-27 15:08:35 +0330

    fix: vercel CLI env

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+
+  env: {
+    NEXT_PUBLIC_REACT_QUERY_RETRY_NUMBER: process.env.NEXT_PUBLIC_REACT_QUERY_RETRY_NUMBER,
+    NEXT_PUBLIC_REACT_QUERY_GC_TIME: process.env.NEXT_PUBLIC_REACT_QUERY_GC_TIME,
+    NEXT_PUBLIC_REACT_QUERY_STALE_TIME: process.env.NEXT_PUBLIC_REACT_QUERY_STALE_TIME,
+    NEXT_PUBLIC_REACT_QUERY_NETWORK_MODE: process.env.NEXT_PUBLIC_REACT_QUERY_NETWORK_MODE,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  },
 };
 
 export default nextConfig;
