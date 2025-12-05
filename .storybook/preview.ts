4e866f55e6ad5597677b99f550021704d0c19c0e
sarinaa-m <sarin.moayedi@gmail.com>
2025-12-05 19:17:39 +0330

    feat: add storybook package

---
+++ .storybook/preview.ts
@@ -0,0 +1,34 @@
+import type { Preview } from '@storybook/nextjs'
+import '../app/globals.css';
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    backgrounds: {
+      default: 'light',
+      values: [
+        {
+          name: 'light',
+          value: '#f9fafb',
+        },
+        {
+          name: 'dark',
+          value: '#111827',
+        },
+      ],
+    },
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: 'todo'
+    }
+  },
+  
+};
+
+export default preview;
